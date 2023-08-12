84ed999c1a8baa6911f2bf2eda0c898fd8fc1da7
Natte2110 <natteparsley@gmail.com>
2023-08-12 17:20:47 +0100

    refactor: Reorganise code and change function type

--- assets/js/script-nav.js
+++ assets/js/script-nav.js
@@ -1,40 +1,38 @@
 var nav = document.getElementById("site-nav");
 var landingImage = document.getElementsByClassName("landing-image")[0];
-window.onscroll = function () { scrollFunction(); };
+var dateFrom = document.getElementById("date-from");
+var dateTo = document.getElementById("date-to");
 
-function scrollFunction() {
+window.onscroll = function () {
     var opacity = 0.8 * (document.documentElement.scrollTop / 480);
     nav.style.backgroundColor = "rgb(51, 51, 51," + opacity + " )";
 
     var blur = 10 * (document.documentElement.scrollTop / 600);
     landingImage.style.filter = "blur(" + blur + "px)";
-}
+};
 
-var dateFrom = document.getElementById("date-from");
-var dateTo = document.getElementById("date-to");
-
-dateTo.addEventListener("change", (event) => {
-    if (event.target.value < dateFrom.value) {
+dateTo.addEventListener("change", function () {
+    if (dateTo.value < dateFrom.value) {
         document.getElementById("warning-to").style.display = "block";
         dateTo.value = "";
-        dateTo.style.border = "solid 1px red"
+        dateTo.style.border = "solid 1px red";
     }
     else {
         document.getElementById("warning-to").style.display = "none";
-        dateTo.style.border = "none"
+        dateTo.style.border = "none";
     }
 });
 
-dateFrom.addEventListener("change", (event) => {
+dateFrom.addEventListener("change", function() {
     if (dateTo.value) {
-        if (event.target.value > dateTo.value) {
+        if (dateFrom.value > dateTo.value) {
             document.getElementById("warning-from").style.display = "block";
             dateFrom.value = "";
-            dateFrom.style.border = "solid 1px red"
+            dateFrom.style.border = "solid 1px red";
         }
         else {
             document.getElementById("warning-from").style.display = "none";
-            dateFrom.style.border = "none"
+            dateFrom.style.border = "none";
         }
     }
 
